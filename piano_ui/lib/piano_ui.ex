c772e5525e53894bef3cb8c08e1aee798e1bc1dc
Jason Axelson <jason.axelson@gmail.com>
2018-12-17 12:37:40 -0800

    Add piano_ui skeleton

---
+++ piano_ui/lib/piano_ui.ex
@@ -0,0 +1,18 @@
+defmodule PianoUi do
+  @moduledoc """
+  Documentation for PianoUi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PianoUi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ piano_ui/test/piano_ui_test.exs
@@ -0,0 +1,8 @@
+defmodule PianoUiTest do
+  use ExUnit.Case
+  doctest PianoUi
+
+  test "greets the world" do
+    assert PianoUi.hello() == :world
+  end
+end
