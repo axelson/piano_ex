cbbb803aac506afb15d4669c90ed39d101c734f7
Jason Axelson <jason.axelson@gmail.com>
2019-01-02 12:00:27 -1000

    Add notes and examples

---
+++ piano_ctl/test/piano_ctl/piano_parser_test.exs
@@ -0,0 +1,13 @@
+defmodule PianoCtl.PianoParserTest do
+  use ExUnit.Case
+
+  # alias PianoCtl.PianoParser
+
+  # describe "parse/1" do
+  #   test "parse" do
+  #     IO.puts "go"
+  #     contents = File.read!("#{__DIR__}/../example_input/songstart-03:58:04:1545101884.txt")
+  #     IO.inspect(contents, label: "contents")
+  #   end
+  # end
+end
