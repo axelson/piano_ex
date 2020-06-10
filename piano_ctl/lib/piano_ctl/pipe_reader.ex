892541cba5b3f82f268d4d84236b4d2b5bce7f1b
Jason Axelson <jason.axelson@gmail.com>
2020-06-09 20:48:15 -1000

    Commit the PipeReader even though it isn't used

---
+++ piano_ctl/lib/piano_ctl/pipe_reader.ex
@@ -0,0 +1,68 @@
+# NOTE: Unused
+defmodule PianoCtl.PipeReader do
+  @moduledoc """
+  Proxy for Pipe IO operations.
+
+  Based on code from:
+  https://elixirforum.com/t/help-with-file-blocking-genserver-process/19646/14
+  """
+  use GenServer
+  require Logger
+
+  def start_link(pipe_name) do
+    GenServer.start_link(__MODULE__, [pipe_name])
+  end
+
+  def close(pipe) do
+    GenServer.stop(pipe, :normal)
+  end
+
+  def read(pipe, amnt) do
+    GenServer.call(pipe, {:read, amnt})
+  end
+
+  def write(pipe, packet) do
+    GenServer.call(pipe, {:write, packet}, :infinity)
+  end
+
+  def init([pipe_name]) do
+    with {_, 0} <- System.cmd("mkfifo", [pipe_name]),
+         pipe <- :erlang.open_port(to_charlist(pipe_name), [:eof, :binary]) do
+      {:ok, %{pipe_name: pipe_name, pipe: pipe, buffer: <<>>, caller: nil, size: nil}}
+    else
+      {:error, _} = error -> {:stop, error}
+      {_, _num} -> {:stop, {:error, "mkfifo"}}
+    end
+  end
+
+  def terminate(_, state) do
+    Logger.warn("PipeWorker #{state.pipe_name} exit")
+    :erlang.port_close(state.pipe)
+    File.rm!(state.pipe_name)
+  end
+
+  def handle_call({:write, packet}, _from, state) do
+    reply = :erlang.port_command(state.pipe, packet)
+    {:reply, reply, state}
+  end
+
+  def handle_call({:read, amnt}, {_pid, ref} = from, %{caller: nil, size: nil} = state) do
+    {:reply, ref, %{state | caller: from, size: amnt, buffer: nil}, {:continue, state.buffer}}
+  end
+
+  def handle_info({pipe, {:data, data}}, %{pipe: pipe, buffer: buffer} = state) do
+    buffer = buffer <> data
+    {:noreply, %{state | buffer: nil}, {:continue, buffer}}
+  end
+
+  def handle_continue(buffer, %{size: size} = state) when byte_size(buffer) >= size do
+    {pid, ref} = state.caller
+    {resp, buffer} = String.split_at(buffer, size)
+    send(pid, {__MODULE__, ref, {:ok, resp}})
+    {:noreply, %{state | caller: nil, size: nil, buffer: buffer}}
+  end
+
+  def handle_continue(buffer, state) do
+    {:noreply, %{state | buffer: buffer}}
+  end
+end
