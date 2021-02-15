674d11de0c682a0221ad228c9b9fa85bee363211
Jason Axelson <jason.axelson@gmail.com>
2021-02-15 09:46:55 -1000

    --no-headers is not available on mac

--- piano_ctl/command.sh
+++ piano_ctl/command.sh
@@ -7,7 +7,7 @@ PID_FILE="${CONFIG_DIR}/piano_ctl_pid"
 
 PID=$(cat "$PID_FILE")
 
-if ps --no-headers "$PID" > /dev/null; then
+if ps "$PID" > /dev/null; then
     echo "$1" >> "$PIPE_FILE"
     echo "$(</dev/stdin)" >> "$PIPE_FILE"
 fi
