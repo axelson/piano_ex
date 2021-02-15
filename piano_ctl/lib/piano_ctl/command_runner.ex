c2ea13342a882a9dd2dd6e4aaea22ed4bd30ef71
Jason Axelson <jason.axelson@gmail.com>
2021-02-15 11:34:22 -1000

    Log commands being run

--- piano_ctl/lib/piano_ctl/command_runner.ex
+++ piano_ctl/lib/piano_ctl/command_runner.ex
@@ -3,6 +3,8 @@ defmodule PianoCtl.CommandRunner do
   Execute commands on Pianobar's control pipe
   """
 
+  require Logger
+
   @commands %{
     :play_pause => "p",
     :next => "n",
@@ -14,10 +16,12 @@ defmodule PianoCtl.CommandRunner do
   def cmd(command) do
     case Map.get(@commands, command) do
       command_string when not is_nil(command_string) ->
+        pipe_path = PianoCtl.Config.control_pipe_path()
+        Logger.info("Sending command string #{inspect(command_string)} to #{inspect(pipe_path)}")
         # `File.write/3` would be more straightforward here but we're not using
         # PianoCtl.PipeReader yet so we need to do this instead
 
-        "echo -n \"#{command_string}\" > #{PianoCtl.Config.control_pipe_path()}"
+        "echo -n \"#{command_string}\" > #{pipe_path}"
         |> to_charlist()
         |> :os.cmd()
     end
--- piano_ui/lib/piano_ui.ex
+++ piano_ui/lib/piano_ui.ex
@@ -4,10 +4,12 @@ defmodule PianoUi do
   """
 
   use Boundary, deps: [PianoCtl, ScenicContrib], exports: []
+  require Logger
 
   def ctl_node, do: Application.fetch_env!(:piano_ui, :ctl_node)
 
   def remote_cmd(command) do
+    Logger.info("Running remote command: #{inspect command}")
     Node.list()
     |> Enum.each(fn node ->
       :rpc.call(node, PianoCtl.Server, :cmd, [command])
