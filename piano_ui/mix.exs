0831fce55db6693c7dd6aed3ef13458e4d2683ff
Jason Axelson <jason.axelson@gmail.com>
2021-02-15 10:08:03 -1000

    Update boundary and pomodoro

--- piano_ui/mix.exs
+++ piano_ui/mix.exs
@@ -23,7 +23,7 @@ defmodule PianoUi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:boundary, "~> 0.7.0", runtime: false},
+      {:boundary, "~> 0.8.0", runtime: false},
       {:libcluster, "~> 3.2.1"},
       {:launcher, github: "axelson/scenic_launcher"},
       {:finch, "~> 0.5"},
